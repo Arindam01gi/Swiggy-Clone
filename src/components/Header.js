a45bd3d23fbed0d3d309817d8c6192171c5bdcb0
Arindam01gi <arindammaiti2018@gmail.com>
2024-08-01 20:11:11 +0530

    header fixing for mobile view

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,8 +6,13 @@ import { useSelector } from "react-redux";
 // import {logo} from '../assets/images/logo'
 const Header = () => {
   const [btnName, setBtnName] = useState("Log in");
-  const [activeHeader,setActiveHeader] = useState("")
+  const [activeHeader, setActiveHeader] = useState("")
+  const [visible, setVisible] = useState(false)
   const location = useLocation();
+
+  const handleClick = () => {
+    setVisible(!visible)
+  }
 
 
 
@@ -18,16 +23,18 @@ const Header = () => {
   const cartItems = useSelector((store) => store.cart.items);
 
   return (
-    <div className="flex justify-between shadow-lg h-[80px] gap-24">
+    <div className="flex justify-between shadow-lg h-[80px] ">
       <div className="justify-center items-center mx-3 p-3 w-1/4 flex ">
         <img className="h-[50px] w-[100px] hover:w-[120px]" src={require("../assets/images/swiggy-logo.svg")} alt="logo" />
       </div>
-      <div className={`nav-items w-3/6 flex justify-center items-center`} >
-        <ul className="flex p-4 m-4">
-          <li className={`px-4 font-semibold hover:text-orange-500 ${activeHeader==='home'?"text-orange-500":""}`}>
+
+      {/* Web Menu */}
+      <div className={`nav-items w-3/6  justify-center items-center hidden md:visible `} >
+        <ul className="p-4 m-4 md:flex  justify-center">
+          <li className={`px-4 font-semibold hover:text-orange-500 ${activeHeader === 'home' ? "text-orange-500" : ""}`}>
             <Link to="/">Home</Link>
           </li>
-          <li className={`px-4 font-semibold hover:text-orange-500 ${activeHeader==='about'?"text-orange-500":""}`}>
+          <li className={`px-4 font-semibold hover:text-orange-500 ${activeHeader === 'about' ? "text-orange-500" : ""}`}>
             <Link to="/about">About Me</Link>
           </li>
           {/* <li className="px-4 font-bold">
@@ -38,10 +45,10 @@ const Header = () => {
           </li> */}
           <li className="px-4 font-bold text-green-500 hover:text-orange-500">
             <Link to="/cart">
-            <span className="p-1">
-              <i className="fas fa-cart-shopping"></i>
-            </span>
-            ({cartItems.length})
+              <span className="p-1">
+                <i className="fas fa-cart-shopping"></i>
+              </span>
+              ({cartItems.length})
             </Link>
           </li>
           <button
@@ -57,6 +64,50 @@ const Header = () => {
           {/* <li className="px-4 ">{data.loggedInUser}</li> */}
         </ul>
       </div>
+
+      {/* Mobile Menu  */}
+      <div className="visible md:hidden relative ">
+        <div className="m-5 pt-2 " onClick={handleClick}>
+          {visible ? <i className="fa fa-xmark"></i>: <i className="fas fa-bars"></i> }
+        </div>
+
+        {
+          visible && <div className={` visible md:invisible  `} >
+            <ul className="absolute bg-white  ">
+              <li className={`px-4 font-semibold hover:text-orange-500 ${activeHeader === 'home' ? "text-orange-500" : ""}`}>
+                <Link to="/">Home</Link>
+              </li>
+              <li className={`px-4 font-semibold hover:text-orange-500 ${activeHeader === 'about' ? "text-orange-500" : ""}`}>
+                <Link to="/about">About Me</Link>
+              </li>
+              <li className="px-4 font-bold text-green-500 hover:text-orange-500">
+                <Link to="/cart">
+                  <span className="p-1">
+                    <i className="fas fa-cart-shopping"></i>
+                  </span>
+                  ({cartItems.length})
+                </Link>
+              </li>
+              <button
+                className="font-semibold hover:text-orange-500"
+                onClick={() => {
+                  btnName === "Log in"
+                    ? setBtnName("Log out")
+                    : setBtnName("Log in");
+                }}
+              >
+                {btnName}
+              </button>
+              {/* <li className="px-4 ">{data.loggedInUser}</li> */}
+            </ul>
+          </div>
+        }
+
+      </div>
+
+
+
+
     </div>
   );
 };
